f6e9aefed49e488e24da75f3cdf577229241259a
dinesh777-dg <dinesh.narayan777@gmail.com>
2026-09-17 06:20:13 +0000

    NodeJS Application initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.write("Hello world from basic node app");
+	res.end();
+});
+
+server.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
